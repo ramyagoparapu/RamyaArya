a526f636bc20075609d115b849ecd38bee933f76
ramyagoparapu@gmail.com <ramyagoparapu@gmail.com>
2022-03-24 13:41:45 -0400

    database

--- src/test/java/runner/Loginrunner.java
+++ src/test/java/runner/Loginrunner.java
@@ -10,7 +10,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="C:\\Users\\vanga\\eclipse-workspace\\database\\src\\test\\java\\features\\Techfiosdatabase.feature",
+		features="src\\test\\java\\features\\Techfiosdatabase.feature",
 		glue="steps",
 		monochrome= true,
 		plugin={
--- src/test/java/stepdefinition/Stepdefntion.java
+++ src/test/java/stepdefinition/Stepdefntion.java
@@ -11,81 +11,59 @@ import database.Loginpage;
 import database.Testbase;
 import junit.framework.Assert;
 
-
-
 public class Stepdefntion extends Testbase {
 	Loginpage lp;
 	Databasepage dbp;
-	
-	
+
 	@Before
 	public void Beforerun() {
 		init();
 		lp = PageFactory.initElements(driver, Loginpage.class);
- dbp=new Databasepage();
-		
-	}
-	// Given user should land on techfios page
-		@Given("^user should land on techfios page$")
-		public void user_should_land_on_techfios_page() {
-			// driver.get("https://techfios.com/billing/?ng=login/");
-			System.out.println("user should land on techfios page");
-		}
+		dbp = new Databasepage();
 
-		@When("^user should enter \"([^\"]*)\" from september2021 database$")
-		public void user_should_enter_from_september2021_database(String Logindata) throws Throwable {
+	}
+
+	// Given user should land on techfios page
+	@Given("^user should land on techfios page$")
+	public void user_should_land_on_techfios_page() {
+		// driver.get("https://techfios.com/billing/?ng=login/");
+		System.out.println("user should land on techfios page");
+	}
+
+	@When("^user should enter \"([^\"]*)\" from september2021 database$")
+	public void user_should_enter_from_september2021_database(String Logindata) throws Throwable {
 		switch (Logindata) {
 		case "username":
-		lp.enterUsername(dbp.takedatafromDB("username"));
-		System.out.println("username from DB:" +dbp.takedatafromDB("username"));
+			lp.enterUsername(dbp.takedatafromDB("username"));
+			System.out.println("username from DB:" + dbp.takedatafromDB("username"));
 			break;
 
-		case "password" :
+		case "password":
 			lp.enterPassword(dbp.takedatafromDB("password"));
-			System.out.println("password from DB:" +dbp.takedatafromDB("password"));
+			System.out.println("password from DB:" + dbp.takedatafromDB("password"));
 			break;
-			default :
-			System.out.println("unable to pull data from DB");
-		}
-		
-		
-		}
-		// When user should click signin button
-		@When("^user should click signin button$")
-		public void user_should_click_signin_button() {
-			lp.clickSignInButton();
-
+		default:
+//			System.out.println("unable to pull data from DB");
 		}
 
-		// Then user should land on dashboard page
-		@Then("^user should land on dashboard page$")
-		public void user_should_land_on_dashboard_page() {
-			String expectedval = "Dashboard- iBilling";
-			String actualval = lp.getPageTitle();
-			Assert.assertEquals(expectedval, actualval);
-			System.out.println("expectedval and actualval both are same ,page matched");
+	}
 
-		}
+	// When user should click signin button
+	@When("^user should click signin button$")
+	public void user_should_click_signin_button() {
+		lp.clickSignInButton();
 
+	}
 
-//	@When("^user should enter \"([^\"]*)\" from september(\\d+) database$")
-//	public void user_should_enter_from_september_database(String Logindata) throws Throwable {
-//	switch (Logindata) {
-//	case "username":
-//	lp.enterUsername(dbp.takedatafromDB("username"));
-//	System.out.println("username from DB:" +dbp.takedatafromDB("username"));
-//		break;
-//
-//	case "password" :
-//		lp.enterUsername(dbp.takedatafromDB("password"));
-//		System.out.println("password from DB:" +dbp.takedatafromDB("password"));
-//		break;
-//		default :
-//		System.out.println("unable to pull data from DB");
-//	}
-//	
-	
+	// Then user should land on dashboard page
+	@Then("^user should land on dashboard page$")
+	public void user_should_land_on_dashboard_page() {
+		String expectedval = "Dashboard- iBilling";
+		String actualval = lp.getPageTitle();
+		Assert.assertEquals(expectedval, actualval);
+		System.out.println("expectedval and actualval both are same ,page matched");
+
 	}
 
 
-	
+}
